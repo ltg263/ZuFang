ee2b90eaca1fd12db148925e967c7f9c3c8bfd77
qianzhanjie <qzj842179561@gmail.com>
2021-07-07 17:08:12 +0800

    初始化

--- app/src/main/java/com/jxxx/zf/api/ApiService.java
+++ app/src/main/java/com/jxxx/zf/api/ApiService.java
@@ -2,17 +2,22 @@ package com.jxxx.zf.api;
 
 
 import com.jxxx.zf.bean.HomeZuFangListBase;
+import com.jxxx.zf.bean.HouseListBase;
 import com.jxxx.zf.bean.Result;
 import com.jxxx.zf.bean.ZuFangDetailsBase;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiService {
 
     @GET("renting/api/v1/home/getHome")
     Observable<Result<HomeZuFangListBase>> homeGetHome();
+
+    @GET("renting/api/v1/house/list")
+    Observable<Result<HouseListBase>> HouseList(@Query("page") int page, @Query("pageSize") int pageSize);
 
     @GET("renting/api/v1/house/details")
     Observable<Result<ZuFangDetailsBase>> houseDetails(@Query("id") String id);
--- app/src/main/java/com/jxxx/zf/view/activity/ZuFangListActivity.java
+++ app/src/main/java/com/jxxx/zf/view/activity/ZuFangListActivity.java
@@ -1,6 +1,8 @@
 package com.jxxx.zf.view.activity;
 
 
+import android.content.ContentValues;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,19 +10,33 @@ import android.widget.PopupWindow;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.jxxx.zf.R;
+import com.jxxx.zf.api.RetrofitUtil;
+import com.jxxx.zf.app.ConstValues;
 import com.jxxx.zf.base.BaseActivity;
+import com.jxxx.zf.bean.HomeZuFangListBase;
+import com.jxxx.zf.bean.HouseListBase;
+import com.jxxx.zf.bean.Result;
 import com.jxxx.zf.utils.RadioGroupSelectUtils;
 import com.jxxx.zf.utils.view.CustomPopWindow;
 import com.jxxx.zf.view.adapter.HomeFyAdapter;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class ZuFangListActivity extends BaseActivity {
 
@@ -34,11 +50,14 @@ public class ZuFangListActivity extends BaseActivity {
     RadioButton mRbHomeSelect4;
     @BindView(R.id.mRadioGroup)
     RadioGroup mMRadioGroup;
+    @BindView(R.id.refreshLayout)
+    SmartRefreshLayout mRefreshLayout;
     @BindView(R.id.rv_home_list)
     RecyclerView mRvHomeList;
     @BindView(R.id.iv_icon)
     ImageView iv_icon;
 
+    int page = 1;
 
     private HomeFyAdapter mHomeFyAdapter;
 
@@ -58,17 +77,7 @@ public class ZuFangListActivity extends BaseActivity {
             }
         });
 
-        new RadioGroupSelectUtils().setOnChangeListener(this,mMRadioGroup,mRbHomeSelect1,mRbHomeSelect2,mRbHomeSelect3,mRbHomeSelect4);
-    }
 
-    @Override
-    public void initData() {
-        List<String> list = new ArrayList<>();
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
         mHomeFyAdapter = new HomeFyAdapter(null);
         mRvHomeList.setAdapter(mHomeFyAdapter);
 
@@ -78,6 +87,69 @@ public class ZuFangListActivity extends BaseActivity {
 
             }
         });
+        new RadioGroupSelectUtils().setOnChangeListener(this,mMRadioGroup,mRbHomeSelect1,mRbHomeSelect2,mRbHomeSelect3,mRbHomeSelect4);
+        mRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+                page = 1;
+                initData();
+            }
+        });
+        mRefreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+                page++;
+                initData();
+            }
+        });
+    }
+
+    @Override
+    public void initData() {
+        RetrofitUtil.getInstance().apiService()
+                .HouseList(page, ConstValues.PAGE_SIZE)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<Result<HouseListBase>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Result<HouseListBase> result) {
+                        hideLoading();
+                        if(isResultOk(result) && result.getData()!=null){
+                            if(result.getData().getList()!=null){
+                                mRefreshLayout.finishRefresh();
+                                mRefreshLayout.finishLoadMore();
+                                if(page==1){
+                                    mHomeFyAdapter.setNewData(result.getData().getList());
+                                } else{
+                                    mHomeFyAdapter.addData(result.getData().getList());
+                                }
+
+                                if(result.getData().getCount()<=mHomeFyAdapter.getData().size()){
+                                    mRefreshLayout.setNoMoreData(true);
+                                }
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        mRefreshLayout.finishRefresh();
+                        mRefreshLayout.finishLoadMore();
+                        hideLoading();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        mRefreshLayout.finishRefresh();
+                        mRefreshLayout.finishLoadMore();
+                        hideLoading();
+                    }
+                });
     }
 
 

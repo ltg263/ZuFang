c09ccc69175842ccee33e014277f4e437313d294
qianzhanjie <qzj842179561@gmail.com>
2021-07-14 10:46:25 +0800

    初始化

--- app/src/main/java/com/jxxx/zf/view/adapter/MineListFylrAdapter.java
+++ app/src/main/java/com/jxxx/zf/view/adapter/MineListFylrAdapter.java
@@ -1,24 +1,59 @@
 package com.jxxx.zf.view.adapter;
 
+import android.content.Intent;
+import android.view.View;
+
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.jxxx.zf.R;
+import com.jxxx.zf.bean.HouseListBase;
+import com.jxxx.zf.bean.ZuFangDetailsBase;
+import com.jxxx.zf.utils.GlideImageLoader;
+import com.jxxx.zf.utils.StringUtil;
+import com.jxxx.zf.view.activity.ZuFangXqActivity;
 
 import java.util.List;
 
-public class MineListFylrAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+public class MineListFylrAdapter extends BaseQuickAdapter<ZuFangDetailsBase, BaseViewHolder> {
     public boolean isBianji = false;
-    public MineListFylrAdapter(List<String> data) {
+    public MineListFylrAdapter(List<ZuFangDetailsBase> data) {
         super(R.layout.item_mine_fylr, data);
     }
 
     @Override
-    protected void convert(BaseViewHolder helper, String item) {
+    protected void convert(BaseViewHolder helper, ZuFangDetailsBase item) {
         helper.addOnClickListener(R.id.bnt_1).addOnClickListener(R.id.bnt_2).addOnClickListener(R.id.bnt_3);
         helper.setGone(R.id.iv_select,false);
         if(isBianji){
             helper.setGone(R.id.iv_select,true);
         }
+
+        GlideImageLoader.loadImageViewRadius(mContext,item.getImgUrl(),helper.getView(R.id.head_icon));
+        helper.setText(R.id.name_type,item.getRentingType().equals("1") ? "合租·" : "整租·" + item.getName())
+                .setText(R.id.year,item.getRentingType().equals("1") ? "合租·" : "整租·" +item.getArea()+"m²·"+ StringUtil.getHouseOrientation(item.getOrientation())+"|"+item.getHousingEstateName())
+                .setText(R.id.tv_llcs,"约看"+item.getViewNum()+"人").setText(R.id.tv_je,item.getRent());
+        helper.setVisible(R.id.tv_lables_1,false).setVisible(R.id.tv_lables_2,false).setGone(R.id.tv_hasVideo,false).setGone(R.id.hasVideo,false);
+        if(item.getHasVideo().equals("1")){
+            helper.setVisible(R.id.hasVideo,true).setVisible(R.id.tv_hasVideo,true);
+        }
+        if (item.getLables() != null) {
+            for (int i = 0; i < item.getLables().size(); i++) {
+                if (i == 0) {
+                    helper.setVisible(R.id.tv_lables_1,true).setText(R.id.tv_lables_1,item.getLables().get(0).getName());
+                }
+                if (i == 1) {
+                    helper.setVisible(R.id.tv_lables_2,true).setText(R.id.tv_lables_2,item.getLables().get(1).getName());
+                }
+            }
+        }
+        helper.getView(R.id.include_yyxx).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mIntent = new Intent(mContext, ZuFangXqActivity.class);
+                mIntent.putExtra("id",item.getId());
+                mContext.startActivity(mIntent);
+            }
+        });
     }
 
     public void setBianji(boolean bianji) {
--- app/src/main/java/com/jxxx/zf/view/fragment/MineFylrFragment.java
+++ app/src/main/java/com/jxxx/zf/view/fragment/MineFylrFragment.java
@@ -6,22 +6,34 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.jxxx.zf.R;
+import com.jxxx.zf.api.RetrofitUtil;
+import com.jxxx.zf.app.ConstValues;
 import com.jxxx.zf.base.BaseFragment;
+import com.jxxx.zf.bean.HouseListBase;
+import com.jxxx.zf.bean.Result;
 import com.jxxx.zf.view.activity.MineFytjActivity;
 import com.jxxx.zf.view.activity.ZuFangFaBuActivity;
 import com.jxxx.zf.view.activity.ZuFangXqActivity;
 import com.jxxx.zf.view.adapter.MineListFylrAdapter;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MineFylrFragment extends BaseFragment {
+    @BindView(R.id.refreshLayout)
+    SmartRefreshLayout mRefreshLayout;
     private MineListFylrAdapter mMineListFylrAdapter;
 
     @BindView(R.id.rv_list)
     RecyclerView mRvList;
+    int page = 1;
     @Override
     protected int setLayoutResourceID() {
         return R.layout.fragment_register;
@@ -29,30 +41,24 @@ public class MineFylrFragment extends BaseFragment {
 
     @Override
     protected void initView() {
-        List<String> list = new ArrayList<>();
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        mMineListFylrAdapter = new MineListFylrAdapter(list);
+        mMineListFylrAdapter = new MineListFylrAdapter(null);
         mRvList.setAdapter(mMineListFylrAdapter);
 
         mMineListFylrAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-                baseStartActivity(ZuFangXqActivity.class,null);
+                baseStartActivity(ZuFangXqActivity.class, null);
             }
         });
         mMineListFylrAdapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
             @Override
             public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
-                switch (view.getId()){
+                switch (view.getId()) {
                     case R.id.bnt_1:
-                        baseStartActivity(MineFytjActivity.class,null);
+                        baseStartActivity(MineFytjActivity.class, null);
                         break;
                     case R.id.bnt_2:
-                        baseStartActivity(ZuFangFaBuActivity.class,null);
+                        baseStartActivity(ZuFangFaBuActivity.class, null);
                         break;
                     case R.id.bnt_3:
 
@@ -63,12 +69,56 @@ public class MineFylrFragment extends BaseFragment {
         });
     }
 
-    public void setNotifyDataSetChanged(boolean isBianJi){
+    public void setNotifyDataSetChanged(boolean isBianJi) {
         mMineListFylrAdapter.setBianji(isBianJi);
         mMineListFylrAdapter.notifyDataSetChanged();
     }
+
     @Override
     protected void initData() {
+        RetrofitUtil.getInstance().apiService()
+                .HouseList(page, ConstValues.PAGE_SIZE, null, null, null, null, null)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<Result<HouseListBase>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
 
+                    }
+
+                    @Override
+                    public void onNext(Result<HouseListBase> result) {
+                        hideLoading();
+                        if (isResultOk(result) && result.getData() != null) {
+                            if (result.getData().getList() != null) {
+                                mRefreshLayout.finishRefresh();
+                                mRefreshLayout.finishLoadMore();
+                                if (page == 1) {
+                                    mMineListFylrAdapter.setNewData(result.getData().getList());
+                                } else {
+                                    mMineListFylrAdapter.addData(result.getData().getList());
+                                }
+
+                                if (result.getData().getCount() <= mMineListFylrAdapter.getData().size()) {
+                                    mRefreshLayout.setNoMoreData(true);
+                                }
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        mRefreshLayout.finishRefresh();
+                        mRefreshLayout.finishLoadMore();
+                        hideLoading();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        mRefreshLayout.finishRefresh();
+                        mRefreshLayout.finishLoadMore();
+                        hideLoading();
+                    }
+                });
     }
 }
